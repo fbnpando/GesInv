c78782cdd26c8cf8622471fdaf9c4a1a17db327a
fbnpando <fpjunivalle@gmail.com>
2024-11-26 12:47:41 -0400

    facturas actuliazas

--- GesInv/Controllers/ProductoesController.cs
+++ GesInv/Controllers/ProductoesController.cs
@@ -183,42 +183,44 @@ namespace GesInv.Controllers
                 PdfDocument document = new PdfDocument();
                 PdfPage page = document.AddPage();
                 XGraphics gfx = XGraphics.FromPdfPage(page);
-
                 // Configurar fuentes y colores
                 XFont titleFont = new XFont("Arial", 20, XFontStyle.Bold);
                 XFont subtitleFont = new XFont("Arial", 14, XFontStyle.Regular);
                 XFont contentFont = new XFont("Arial", 12, XFontStyle.Regular);
-                XBrush headerBrush = new XSolidBrush(XColor.FromArgb(0xFF7986CB)); // Azul oscuro
-                XBrush bodyBrush = new XSolidBrush(XColor.FromArgb(0xFFE8EAF6)); // Azul claro
-
-                // Cargar el logotipo
+                XBrush headerBrush = new XSolidBrush(XColor.FromArgb(0xFF00A09F)); // Verde turquesa
+                XBrush bodyBrush = new XSolidBrush(XColor.FromArgb(0xFFFFFFFF)); // Blanco
+                                                                                 // Cargar el logotipo
                 XImage logoImage = XImage.FromFile("wwwroot/images/logo.png");
-
                 // Dibujar el encabezado
                 gfx.DrawRectangle(headerBrush, 0, 0, page.Width, 150);
-                gfx.DrawString("FACTURA", titleFont, XBrushes.White, new XPoint(50, 50));
-                gfx.DrawString("GestionInventario Inc.", subtitleFont, XBrushes.White, new XPoint(50, 90));
-                gfx.DrawString("Carretera Muelle 38, 37531 Avila, Avila", contentFont, XBrushes.White, new XPoint(50, 120));
-                gfx.DrawImage(logoImage, 500, 20, 100, 50); // Dibujar el logotipo
-
+                gfx.DrawImage(logoImage, 50, 20, 100, 50); // Dibujar el logotipo
+                gfx.DrawString("FACTURA", titleFont, XBrushes.White, new XPoint(200, 50));
+                gfx.DrawString("Fecha: 12/ENERO/2021", contentFont, XBrushes.White, new XPoint(200, 90));
+                gfx.DrawString("Factura: 003", contentFont, XBrushes.White, new XPoint(200, 120));
+                // Dibujar la información del cliente
+                gfx.DrawString("Fabian Pando Justiniano", subtitleFont, XBrushes.Black, new XPoint(50, 200));
+                gfx.DrawString("Produtos", contentFont, XBrushes.Black, new XPoint(50, 220));
+                gfx.DrawString("(55) 1235-4567", contentFont, XBrushes.Black, new XPoint(50, 240));
+                gfx.DrawString("Calle Flora 123, Ciudad de México", contentFont, XBrushes.Black, new XPoint(50, 260));
                 // Dibujar la información del producto
-                // Dibujar la información del producto
-                gfx.DrawRectangle(bodyBrush, 50, 200, page.Width - 100, 250);
+                gfx.DrawRectangle(bodyBrush, 50, 300, page.Width - 100, 250);
                 DrawWatermark(gfx, page);
-
-                // Aquí se obtiene el nombre y demás detalles del producto usando la instancia "producto"
-                gfx.DrawString($"Producto: {producto.Nombre}", subtitleFont, XBrushes.Black, new XPoint(100, 230));
-                gfx.DrawString($"Descripción: {producto.Descripcion}", contentFont, XBrushes.Black, new XPoint(100, 260));
-                gfx.DrawString($"Precio: ${producto.Precio.ToString("F2")}", contentFont, XBrushes.Black, new XPoint(100, 290));
-                gfx.DrawString($"Stock: {producto.Stock}", contentFont, XBrushes.Black, new XPoint(100, 320));
-
-
+                gfx.DrawString($"Producto: {producto.Nombre}", subtitleFont, XBrushes.Black, new XPoint(100, 330));
+                gfx.DrawString($"Categoría: {producto.Categoria.Nombre}", contentFont, XBrushes.Black, new XPoint(100, 360));
+                gfx.DrawString($"Proveedor: {producto.Proveedor.Nombre}", contentFont, XBrushes.Black, new XPoint(100, 390));
+                gfx.DrawString($"Descripción: {producto.Descripcion}", contentFont, XBrushes.Black, new XPoint(100, 420));
+                gfx.DrawString($"Precio: ${producto.Precio.ToString("F2")}", contentFont, XBrushes.Black, new XPoint(100, 450));
+                gfx.DrawString($"Stock: {producto.Stock}", contentFont, XBrushes.Black, new XPoint(100, 480));
+                // Dibujar el método de pago y el total
+                gfx.DrawString("Método de pago", contentFont, XBrushes.Black, new XPoint(50, 570));
+                gfx.DrawString("CUENTA: 4916 8418 9082 4339", contentFont, XBrushes.Black, new XPoint(50, 590));
+                gfx.DrawString($"TOTAL: ${producto.Precio.ToString("F2")}", titleFont, XBrushes.Black, new XPoint(50, 620));
                 // Dibujar el pie de página
                 XFont footerFont = new XFont("Arial", 10, XFontStyle.Italic);
-                gfx.DrawString("Gracias por su compra.", footerFont, XBrushes.Black, new XPoint(50, page.Height - 50));
-
+                gfx.DrawString("¿Alguna pregunta? Envíanos un correo a hola@sitioincreible.mx", footerFont, XBrushes.Black, new XPoint(50, page.Height - 50));
+                gfx.DrawString("¡GRACIAS!", footerFont, XBrushes.Black, new XPoint(50, page.Height - 30));
+                gfx.DrawString("Av. Libertadores 123 Tehuacán, Puebla · sitioincreible.mx", footerFont, XBrushes.Black, new XPoint(50, page.Height - 10));
                 // Dibujar la marca de agua
-
                 document.Save(memoryStream, false);
                 memoryStream.Position = 0;
                 return File(memoryStream.ToArray(), "application/pdf", "Factura_" + producto.Nombre + ".pdf");
@@ -230,14 +232,11 @@ namespace GesInv.Controllers
             string watermarkText = "GestionInventario";
             XFont watermarkFont = new XFont("Arial", 80, XFontStyle.Bold);
             XBrush watermarkBrush = new XSolidBrush(XColor.FromArgb(0xFFD0D0D0)); // Color gris claro
-
             double angle = -45;
             gfx.RotateTransform(angle);
-
             // Establecer la posición de la marca de agua
             // Colocamos la marca de agua más abajo en la página, después del contenido.
-            gfx.DrawString(watermarkText, watermarkFont, watermarkBrush, new XPoint(-350, page.Height -350)); // Ajusta el valor 150 según sea necesario
-
+            gfx.DrawString(watermarkText, watermarkFont, watermarkBrush, new XPoint(-350, page.Height - 350)); // Ajusta el valor 150 según sea necesario
             gfx.RotateTransform(-angle);
         }
 
